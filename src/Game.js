3c2d79d6d78d53d1c00a277ba5038fdef1ac9c5c
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2019-11-05 16:25:15 +0100

    game over ready

--- src/Game.js
+++ src/Game.js
@@ -18,6 +18,10 @@ Game.prototype.start = function() {
   this.canvas = document.querySelector('canvas');
   this.ctx = this.canvas.getContext('2d');
 
+  // Save reference to the score and lives elements
+  this.livesElement = this.gameScreen.querySelector('.lives .value');
+  this.scoreElement = this.gameScreen.querySelector('.score .value');
+
   // Set the canvas to be same as the viewport size
   this.containerWidth = this.canvasContainer.offsetWidth;
   this.containerHeight = this.canvasContainer.offsetHeight;
@@ -32,7 +36,7 @@ Game.prototype.start = function() {
   
   // creates enemies
     for (var i=0; i<5; i++){
-      var newEnemy = new Enemy(this.canvas, 75*i, 100, 1,1);
+      var newEnemy = new Enemy(this.canvas, 75*i, 600, 1,1);
       this.enemies.push(newEnemy);
       
     }
@@ -70,6 +74,7 @@ Game.prototype.start = function() {
   this.startLoop();
 };
 
+///////////// LOOOOP STARTS HERE //////////////////
 
 Game.prototype.startLoop = function() {
   var loop = function() {
@@ -102,6 +107,10 @@ Game.prototype.startLoop = function() {
               if ((this.projectile.y + (this.projectile.size / 2)) < 0 ) { this.projectile=null;}
         }
 
+
+    // 7. check if Aliens landed
+    this.Alienlanded();
+
 // 2. CLEAR THE CANVAS
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -116,9 +125,6 @@ Game.prototype.startLoop = function() {
 // 3. UPDATE THE CANVAS
     // Draw the player
     this.player.draw();
-    // Draw points and lives ;
-   // this.context.fillText('Lifes : ' ,  100, 40);
-   // this.context.fillText('points : ', 100, 60);
     
 
    if ( this.projectile!=null) { 
@@ -126,9 +132,9 @@ Game.prototype.startLoop = function() {
     this.checkProjectileCollisions();
   }
     
-    //this.alien1.draw();
+    
     this.enemies.forEach(function(enemy) {
-      //console.log(enemy.live);
+      
       if(enemy.live===1) {enemy.draw();}
     });
     
@@ -140,7 +146,9 @@ Game.prototype.startLoop = function() {
     if (!this.gameIsOver) {
       window.requestAnimationFrame(loop)
     }
-  
+    
+    //update game stats
+    this.updateGameStats();	
    // window.requestAnimationFrame(loop);
   }.bind(this);
 
@@ -149,7 +157,7 @@ Game.prototype.startLoop = function() {
 
 
 
-
+// CHECK ENEMIES COLIDING THE SCREEN THEN MOVE DOWN
 
 
 Game.prototype.checkEnemiesScreenCollision = function(){
@@ -180,6 +188,8 @@ Game.prototype.checkEnemiesScreenCollision = function(){
   }
 }
 
+// CHECK COLLISIONS BETWEEN PROJECTILES AND ENEMIES
+
 Game.prototype.checkProjectileCollisions = function() {
   
   this.enemies.forEach( function(enemy) {
@@ -194,7 +204,7 @@ Game.prototype.checkProjectileCollisions = function() {
           this.projectile.y = 0 - this.projectile.size;
 
           enemy.live = 0;
-          //enemy=null;
+          
 
           /*
           if (this.player.lives === 0) {
@@ -202,12 +212,54 @@ Game.prototype.checkProjectileCollisions = function() {
           }*/
           console.log("enemy hit");
 
-          //this.projectile=null;
+          
       }
     }
   }, this);
   // We have to pass `this` value as the second argument
   // as array method callbacks have a default `this` of undefined.
+};
+
+// aliens landing
+
+Game.prototype.Alienlanded = function () {
+  
+    if ( this.enemies[0].y >= this.player.y ){
+      this.gameOver();
+    }
+
+}
+
+
+
+
+// gameOver()
+
+Game.prototype.updateGameStats = function() {
+  this.score += 1;
+  this.livesElement.innerHTML = this.player.lives;
+  this.scoreElement.innerHTML = this.score;
+};
+
+Game.prototype.passGameOverCallback = function(gameOver) {
+  this.onGameOverCallback = gameOver;
+};
+
+
+
+Game.prototype.gameOver = function() {
+  // flag `gameIsOver = true` stops the loop
+  this.gameIsOver = true;
+  console.log('GAME OVER');
+  
+  // Call the gameOver function from `main` to show the Game Over Screen
+  this.onGameOverCallback();
+};
+
+//	game.js	 removeGameScreen()
+
+Game.prototype.removeGameScreen = function() {
+  this.gameScreen.remove(); // remove() is the DOM method which removes the DOM Node  
 };
 
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// Creates DOM elements from a string representation
+
 function buildDom(htmlString) {
   const div = document.createElement('div');
   div.innerHTML = htmlString;
@@ -6,7 +10,14 @@ function buildDom(htmlString) {
 }
 
 function main() {
-  var splashScreen;
+  
+  var game; // instance of the Game
+  var splashScreen; // Start Screen
+  var gameScreen;
+  var gameOverScreen;
+
+    
+  
 
   // -- splash screen
   function createSplashScreen() {
@@ -20,7 +31,7 @@ function main() {
     document.body.appendChild(splashScreen);
 
     var startButton = splashScreen.querySelector('button');
-      startButton.addEventListener('click', function() {
+    startButton.addEventListener('click', function() {
           startGame();
       });
   }
@@ -30,9 +41,20 @@ function main() {
   }
   
   // -- game screen
+
   function createGameScreen() {
-    var gameScreen = buildDom(`
+    gameScreen = buildDom(`
       <main class="game">
+      <header>
+        <div class="lives">
+          <span class="label">Lives:</span>
+          <span class="value"></span>
+        </div>
+        <div class="score">
+          <span class="label">Score:</span>
+          <span class="value"></span>
+        </div>
+      </header>
         
         <section class="canvas-container">
           <canvas></canvas>
@@ -45,19 +67,69 @@ function main() {
     return gameScreen;
   }
   
+ 
+  // setiing game over screen
+
+  function createGameOverScreen(score) {
+    gameOverScreen = buildDom(`
+    <main>
+      <h1>Game over</h1>
+      <p>Your score: <span></span></p>
+      <button>Restart</button>
+    </main>
+  `);
+
+    var button = gameOverScreen.querySelector('button');
+    button.addEventListener('click', startGame);
+
+    var span = gameOverScreen.querySelector('span');
+    span.innerText = score;
+
+    document.body.appendChild(gameOverScreen);
+  }
+
+  function removeGameOverScreen() {
+    if (gameOverScreen !== undefined) {
+        gameOverScreen.remove();
+      }
+  }
+
+
+
   // -- Setting the game state 
 
   function startGame() {
     removeSplashScreen();
+    removeGameOverScreen();	
 
     var game = new Game();
     game.gameScreen = createGameScreen();
 
     game.start();
+    // End the game
+    game.passGameOverCallback( function() {		// <-- UPDATE
+      gameOver(game.score);					// <-- UPDATE
+    });
   }
+
+  // main.js		gameOver()
+
+  function gameOver(score) {
+    removeGameScreen();
+    createGameOverScreen(score);
+  }
+  
+  // removeGameScreen() - inside main()
+  
+  function removeGameScreen() {
+    gameScreen.remove()
+  }
+
 
   // -- initialize Splash screen on initial start
   createSplashScreen();
 }
 
-window.onload = main;
+//window.onload = main;
+// Runs the function `main` once all resources are loaded
+window.addEventListener('load', main);
